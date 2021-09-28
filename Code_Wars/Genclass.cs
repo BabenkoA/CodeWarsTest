0659c31985e4baa9672d5dbd718813de76a5416e
Andrew Babenko <smaeel201356@gmail.com>
2021-09-28 16:00:33 +0300

    added class

--- Code_Wars/Genclass.cs
+++ Code_Wars/Genclass.cs
@@ -26,5 +26,10 @@ namespace Code_Wars
         {
             Array.Resize<T>(ref arr, Length - 1);
         }
+
+        public T Find() 
+        {
+            return default(T);
+        }
     }
 }
