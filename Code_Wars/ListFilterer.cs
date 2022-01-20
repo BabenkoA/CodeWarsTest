80d2e70382e0ef0660ca569f5c4fb5fe32be76f9
Andrew Babenko <smaeel201356@gmail.com>
2022-01-20 10:50:19 +0200

    added new class

---
+++ Code_Wars/ListFilterer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Wars
+{
+   public class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            return (IEnumerable<int>)listOfItems.OfType<int>().ToList();
+        }
+    }
+}
